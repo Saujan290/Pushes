8768e13d0df341a1b29c470b7239337facfe9b44
Saujan290 <kafle.saujan@gmail.com>
2015-05-27 14:32:37 -0400

    Disulfide

---
+++ HackaMol-CaseStudy/Vina_disulfide/prepare_receptors/prepare_receptor4.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env /Library/MGLTools/latest/bin/pythonsh
+# THIS IS JUST A PLACE HOLDER FOR THE SCRIPT!  To get the script, please install MGLTools
+# 
+# the path in the shebang line is where it is on my OS X machine
